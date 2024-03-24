a72980ed4f37e49843e93e91225c32aba12ebe42
Stepan K <stepan.korevanov@mail.ru>
2024-03-24 10:53:03 +0300

    homework_8

---
+++ 08.homework/build/CMakeFiles/topk_words.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/topk_words.dir/main.cpp.o"
+  "topk_words"
+  "topk_words.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/topk_words.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
